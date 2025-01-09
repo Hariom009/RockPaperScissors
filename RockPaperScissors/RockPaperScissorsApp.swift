db0cd2d220a6157e0f9526775a75cf98a8e99a37
Hariom <hariomconnects03@gmail.com>
2025-01-10 00:55:02 +0530

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Hari's Mac on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsTests.swift
+//  RockPaperScissorsTests
+//
+//  Created by Hari's Mac on 10.01.2025.
+//
+
+import Testing
+@testable import RockPaperScissors
+
+struct RockPaperScissorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
